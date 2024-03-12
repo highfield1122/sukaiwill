ed0bbf05c88c0d7eb7ac41cc3bde853a28716c8f
highfield <elciart1011@gmail.com>
2024-03-12 14:44:32 +0900

    popup機能に変更

--- startrpg.py
+++ startrpg.py
@@ -1,9 +1,14 @@
+"""
+ゲーム名を始める
+プレイヤー名を決定する
+"""
+import PySimpleGUI as sg
 def main():
     msg='名前を教えてください：'
     name=input(msg)
     msg='ようこそ'+name
-    print(msg,flush=True)
-    print('出発しましょう'+name)
+    sg.popup(msg)
+    print('出発しましょう、'+name)
 
 if __name__=='__main__':
     main()
