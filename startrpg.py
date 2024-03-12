116f31ea75b09450145cc4dfb2467145a897b3e1
highfield <elciart1011@gmail.com>
2024-03-12 14:14:52 +0900

    initial commit

--- startrpg.py
+++ startrpg.py
@@ -1,8 +1,9 @@
 def main():
-    msg='名前を教えてください。'
+    msg='名前を教えてください：'
     name=input(msg)
     msg='ようこそ'+name
-    print('出発'+name)
+    print(msg)
+    print('出発しましょう'+name)
 
 if __name__=='__main__':
     main()
